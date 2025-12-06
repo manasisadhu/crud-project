d52f5a6f377fbadb57a5865bdd25deb91c667ef4
manasisadhu <manasisadhu57@gmail.com>
2025-12-06 19:02:19 +0530

    teacher form created

--- src/app/teacher/create/page.tsx
+++ src/app/teacher/create/page.tsx
@@ -1,5 +1,15 @@
+import TeacherForm from "@/components/Forms/TeacherForm";
+import { Card } from "@/components/shadcnui/card";
+
 const page = () => {
-	return <div>Enter</div>;
+	return (
+		<section className="grid h-dvh place-items-center">
+			<Card className="w-[320] p-5">
+				<h1 className="text-center text-2xl font-bold">Create Teacher</h1>
+				<TeacherForm />
+			</Card>
+		</section>
+	);
 };
 
 export default page;
--- src/components/Forms/TeacherForm.tsx
+++ src/components/Forms/TeacherForm.tsx
@@ -1,7 +1,82 @@
 "use client";
 
+import { teacherSchema, TeacherSchemaType } from "@/lib/Schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InfoIcon, Loader2Icon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const TeacherForm = () => {
-	return <div>Enter</div>;
+	const { handleSubmit, control, formState } = useForm({
+		resolver: zodResolver(teacherSchema),
+		defaultValues: {
+			firstName: "",
+			lastName: "",
+		},
+		mode: "onSubmit",
+	});
+
+	const submitTeacherData = (fdata: TeacherSchemaType) => {
+		console.log(fdata);
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit(submitTeacherData)}
+			className="space-y-3">
+			<Controller
+				name="firstName"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>first name</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							type="text"
+							aria-invalid={fieldState.invalid}
+							placeholder="enter your first name"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="lastName"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>last name</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							type="text"
+							aria-invalid={fieldState.invalid}
+							placeholder="enter your last name"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+			<Button
+				disabled={formState.isSubmitting}
+				className="w-full">
+				{formState.isSubmitting ? (
+					<>
+						<Loader2Icon className="animate-spin" />
+						submitting.....
+					</>
+				) : (
+					<>
+						<InfoIcon /> submit
+					</>
+				)}
+			</Button>
+		</form>
+	);
 };
 
 export default TeacherForm;
