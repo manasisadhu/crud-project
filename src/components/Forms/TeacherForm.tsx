8252fe9d426b16848e58178ae665e4ecab7cb0e1
manasisadhu <manasisadhu57@gmail.com>
2025-12-10 09:10:02 +0530

    Add teacher details generator to TeacherForm
    Introduced a button in TeacherForm to auto-generate teacher first and last names using @faker-js/faker. Updated dependencies and added loading state for the generator button to improve user experience.

--- src/components/Forms/TeacherForm.tsx
+++ src/components/Forms/TeacherForm.tsx
@@ -2,8 +2,10 @@
 
 import { teacherSchema, TeacherSchemaType } from "@/lib/Schema";
 import createTeacherAction from "@/server/createTeacherAction";
+import { faker } from "@faker-js/faker/locale/en_IN";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { InfoIcon, Loader2Icon } from "lucide-react";
+import { InfoIcon, Loader2Icon, Sparkles } from "lucide-react";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { Button } from "../shadcnui/button";
@@ -11,14 +13,16 @@ import { Field, FieldError, FieldLabel } from "../shadcnui/field";
 import { Input } from "../shadcnui/input";
 
 const TeacherForm = () => {
-	const { handleSubmit, control, formState, reset } = useForm({
-		resolver: zodResolver(teacherSchema),
-		defaultValues: {
-			firstName: "",
-			lastName: "",
-		},
-		mode: "onSubmit",
-	});
+	const [isLoading, setIsLoading] = useState(false);
+	const { handleSubmit, control, formState, reset, setValue, clearErrors } =
+		useForm({
+			resolver: zodResolver(teacherSchema),
+			defaultValues: {
+				firstName: "",
+				lastName: "",
+			},
+			mode: "onSubmit",
+		});
 
 	const submitTeacherData = async (fdata: TeacherSchemaType) => {
 		await new Promise<void>((r) => setTimeout(r, 1800));
@@ -32,63 +36,93 @@ const TeacherForm = () => {
 			toast.error(message);
 		}
 	};
+	const teacherDetailsGenaretor = async () => {
+		setIsLoading(true);
+		await new Promise<void>((r) => setTimeout(r, 1800));
+
+		const { person } = faker;
+
+		setValue("firstName", person.firstName());
+		setValue("lastName", person.lastName());
+		clearErrors();
+		setIsLoading(false);
+	};
 
 	return (
-		<form
-			onSubmit={handleSubmit(submitTeacherData)}
-			className="space-y-3">
-			<Controller
-				name="firstName"
-				control={control}
-				render={({ field, fieldState }) => (
-					<Field data-invalid={fieldState.invalid}>
-						<FieldLabel htmlFor={field.name}>first name</FieldLabel>
-						<Input
-							{...field}
-							id={field.name}
-							type="text"
-							aria-invalid={fieldState.invalid}
-							placeholder="enter your first name"
-							autoComplete="given-name"
-						/>
-						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-					</Field>
-				)}
-			/>
+		<>
+			<form
+				onSubmit={handleSubmit(submitTeacherData)}
+				className="space-y-3">
+				<Controller
+					name="firstName"
+					control={control}
+					render={({ field, fieldState }) => (
+						<Field data-invalid={fieldState.invalid}>
+							<FieldLabel htmlFor={field.name}>first name</FieldLabel>
+							<Input
+								{...field}
+								id={field.name}
+								type="text"
+								aria-invalid={fieldState.invalid}
+								placeholder="enter your first name"
+								autoComplete="given-name"
+							/>
+							{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+						</Field>
+					)}
+				/>
 
-			<Controller
-				name="lastName"
-				control={control}
-				render={({ field, fieldState }) => (
-					<Field data-invalid={fieldState.invalid}>
-						<FieldLabel htmlFor={field.name}>last name</FieldLabel>
-						<Input
-							{...field}
-							id={field.name}
-							type="text"
-							aria-invalid={fieldState.invalid}
-							placeholder="enter your last name"
-							autoComplete="given-name"
-						/>
-						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-					</Field>
-				)}
-			/>
+				<Controller
+					name="lastName"
+					control={control}
+					render={({ field, fieldState }) => (
+						<Field data-invalid={fieldState.invalid}>
+							<FieldLabel htmlFor={field.name}>last name</FieldLabel>
+							<Input
+								{...field}
+								id={field.name}
+								type="text"
+								aria-invalid={fieldState.invalid}
+								placeholder="enter your last name"
+								autoComplete="given-name"
+							/>
+							{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+						</Field>
+					)}
+				/>
+				<Button
+					type="submit"
+					disabled={formState.isSubmitting}
+					className="w-full cursor-pointer">
+					{formState.isSubmitting ? (
+						<>
+							<Loader2Icon className="animate-spin" />
+							submitting.....
+						</>
+					) : (
+						<>
+							<InfoIcon /> submit
+						</>
+					)}
+				</Button>
+			</form>
 			<Button
-				disabled={formState.isSubmitting}
-				className="w-full">
-				{formState.isSubmitting ? (
+				className="cursor-pointer"
+				onClick={teacherDetailsGenaretor}
+				type="button"
+				disabled={isLoading}>
+				{isLoading ? (
 					<>
 						<Loader2Icon className="animate-spin" />
-						submitting.....
+						Genarating......
 					</>
 				) : (
 					<>
-						<InfoIcon /> submit
+						<Sparkles /> Genarate
 					</>
 				)}
 			</Button>
-		</form>
+		</>
 	);
 };
 
